0d636de9cc48ec33ada6f44bce1a39cd55622618
Yudzhyn <Snya14@gmail.com>
2022-06-17 11:55:08 +0300

    Realize UnderOverState class for changing state bet.

--- strategy/six_losses.py
+++ strategy/six_losses.py
@@ -1,4 +1,4 @@
-from strategy.strategy import Strategy
+from strategy.strategy import Strategy, UnderOverState
 from auto_bot.bet_data import BetData
 from other.calculates import percentage
 
@@ -8,11 +8,15 @@ class SixLossesStrategy(Strategy):
     def __init__(self, payout: float = 1.3026):
         self.__payout: float = payout
         self.__increase_coef_percent: int = 300
+        self.__state = UnderOverState(2)
 
     def init_bet_data(self, bet_feedback_data: BetData) -> None:
         bet_feedback_data.payout = self.__payout
+        self.__state.init_state(bet_feedback_data.under_over)
+        bet_feedback_data.under_over = self.__state.next_state()
 
     def calculate_if_lose(self, bet_feedback_data: BetData) -> None:
+        bet_feedback_data.under_over = self.__state.next_state()
         if bet_feedback_data.number_losses_from_last_win >= 6:
             bet_feedback_data.bet = bet_feedback_data.base_bet
             bet_feedback_data.number_losses_from_last_win = 0
@@ -21,6 +25,7 @@ class SixLossesStrategy(Strategy):
                                                self.__increase_coef_percent)
 
     def calculate_if_win(self, bet_feedback_data: BetData) -> None:
+        bet_feedback_data.under_over = self.__state.next_state()
         bet_feedback_data.bet = bet_feedback_data.base_bet
 
     def __repr__(self) -> str:
--- strategy/strategy.py
+++ strategy/strategy.py
@@ -1,5 +1,6 @@
 import abc
 from auto_bot.bet_data import BetData
+from typing import List, Optional
 
 
 class Strategy(abc.ABC):
@@ -23,3 +24,28 @@ class Strategy(abc.ABC):
     @abc.abstractmethod
     def __str__(self):
         """Calculate parameters of bet data if win"""
+
+
+class UnderOverState:
+    __slots__ = ["__state", "__number_change", "__states", "__internal_counter"]
+
+    def __init__(self, number_change: int = 2):
+        self.__state: Optional[bool] = None
+        self.__states: Optional[List[bool]] = None
+        self.__number_change: int = number_change
+        self.__internal_counter: int = 0
+
+    def init_state(self, state) -> None:
+        self.__state: bool = state
+        self.__states: List[bool] = \
+            [state] * self.__number_change + [not state] * self.__number_change
+
+    def next_state(self) -> bool:
+        if not (self.__state or self.__states):
+            raise ValueError("The state isn't initialized.")
+
+        _state = self.__states[self.__internal_counter]
+        self.__internal_counter += 1
+        if self.__internal_counter >= len(self.__states):
+            self.__internal_counter = 0
+        return _state
